254b4ff25751c1a9a0dd87f1a40d4df9371152d7
tanuja.gupta0504 <tanuja55@yahoo.com>
2019-08-24 22:00:15 +0530

    Most Frequent Elements

---
+++ code/src/searching/TopKFrequent.java
@@ -0,0 +1,11 @@
+package searching;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TopKFrequent {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> list = new LinkedList<>();
+        return list;
+    }
+}
