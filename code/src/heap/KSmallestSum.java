7b77cf2dad244f425b7eba9d7db44f056c063831
tanuja.gupta0504 <tanuja55@yahoo.com>
2020-06-27 18:19:09 +0530

    Leetcode Challenges

---
+++ code/src/heap/KSmallestSum.java
@@ -0,0 +1,11 @@
+package heap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KSmallestSum {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        return result;
+    }
+}
